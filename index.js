7b86e567b10d5c8fcc0b476aa85f16a60fe24ebe
Mikell <Mikellt@live.com>
2022-07-28 01:11:32 -0700

    stuck on index.js

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "what",
+        type: "input",
+        message: "what is the app for?",
+    },
+    {
+        name: "how",
+        type: "input",
+        message: "How do I use the app?",
+    },
+    {
+        name: "install",
+        type: "input",
+        message: "how do I install it?",
+    },
+    {
+        name: "issues",
+        type: "input",
+        message: "how do I report issues?",
+    },
+    {
+        name: "contribution",
+        type: "input",
+        message: "how do I make contributions?",
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("README.md", fileName, (err) => err ? console.error(err): console.log("success"))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answer)=>{
+        console.log("saved")
+    })
+    const newFile = generateReadMe(question); 
+}
 
 // Function call to initialize app
 init();
