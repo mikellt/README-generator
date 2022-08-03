1bca962c1159ce03e0271a7d5d8cdd6caa4af54d
Mikell <Mikellt@live.com>
2022-08-02 23:58:14 -0700

    reformated gerneated mark down js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license){
-    return (license)
-  } else {
-    return("")
-  }
+// function renderLicenseBadge(license) {}
+
+const renderLicenseBadge = (license) => {
+  if(license)
+
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
+
+const renderLicenseLink = (license) => {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
+
+const renderLicenseSection = (license) => {
+
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function generateMarkdown(data) {}
+const generateMarkdown = (data) => {}
 
-`;
-}
 
 module.exports = generateMarkdown;
